b70218cbb9eb46180a77c00320906824efa404a6
Diego Muro <diegogmuro@hotmail.com>
2022-12-09 17:49:42 -0300

    actualizacion --leer mas--

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+
+let readMore_btn = document.getElementById('readMore_btn');
+let hideText = document.getElementById('hideText');
+
+readMore_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+    hideText.classList.toggle('showText');
+
+    if(hideText.classList.contains('showText')) {
+    readMore_btn.innerHTML = 'Leer menos'
+    }
+    else {
+    readMore_btn.innerHTML = 'Leer mas'
+    }
+}
+
